48a0f5de91d2e51c254e9829eb52129a7d95b7c6
pianoman373 <jholley373@yahoo.com>
2016-12-10 03:18:28 -0600

    No more flying tiles.

--- LD-src/com/hact/ld/Tile.java
+++ LD-src/com/hact/ld/Tile.java
@@ -10,8 +10,4 @@ public class Tile extends MeshObject {
 	public Tile(Vec3 pos) {
 		super(pos, vec4(0, 1, 0, 0), Main.tileMesh, 1, new Material(vec3(1, 1, 1), 0.5f, 0));
 	}
-	
-	public void update() {
-		this.setPosition(this.getPosition().add(vec3(0, 0.1f, 0)));
-	}
 }
